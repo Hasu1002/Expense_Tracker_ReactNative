b9ef8bbf2c89628eb3901a5ce0f76723d63cc9d0
Harshavardhini <harshavardhini2304@gmail.com>
2024-08-19 10:05:41 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function App() {
+  return (
+    <View className="bg-red-400">
+      <Text className="text-white">Hello World!!</Text>
+    </View>
+  );
+}
+
+export default App;
